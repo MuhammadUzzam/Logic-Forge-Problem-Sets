420bed2e72adcfe9758f62ac071fdb8be9df5457
Uzzam <muhammaduzzams2007@gmail.com>
2026-01-07 05:10:53 +0500

    Add optimized solution and approach for password recovery window

---
+++ Logic Forge – Week 1/Challenge 02/password_recovery_window_optimized.py
@@ -0,0 +1,27 @@
+log=str(input())
+pattern=str(input())
+hsh=dict()
+for ch in pattern:
+    if ch in hsh:
+        hsh[ch]+=1
+    else:
+        hsh[ch]=1
+need=len(hsh)
+lo=0
+mn=1e18
+ans=""
+for r in range(len(log)):
+    if log[r] in hsh:
+        hsh[log[r]]-=1
+        if hsh[log[r]]==0:
+            need-=1
+    while need==0:
+        if r-l+1<mn:
+            mn=r-l+1
+            ans=log[l:r+1]
+        if log[l] in hsh:
+            hsh[log[l]]+=1
+            if hsh[log[l]]==1:
+                need+=1
+        l+=1
+print(ans)
